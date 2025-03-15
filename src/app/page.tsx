f452f873d584f9fb4a631f76bed7118c5bb64f79
dosqas <137123562+dosqas@users.noreply.github.com>
2025-03-15 18:55:37 +0200

    Created the common layout header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-
-const Home: React.FC = () => {
-  const [songs, setSongs] = useState<{ id: number; title: string; album: string; artist: string; genre: string }[]>([]);
-
-  useEffect(() => {
-    fetch('/api/songs')
-      .then((response) => response.json())
-      .then((data) => setSongs(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
+function HomePage() {
   return (
     <div>
-      <h1 className="p-8">J Dilla - Donuts (Tracklist)</h1>
-      <ul>
-        {songs.map((song) => (
-          <li key={song.id}>
-            <strong>{song.title}</strong> | <em>{song.album}</em> | <em>{song.artist}</em> | {song.genre}
-          </li>
-        ))}
-      </ul>
+      <p>Your web music tracker with stats & analytics.</p>
     </div>
   );
-};
+}
 
-export default Home;
+export default HomePage;
---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Header: React.FC = () => {
+  return (
+    <header className="site-header">
+      <div className="site-header-content">
+        <div className="site-header-logo">
+          <Link href="/">
+            <span className="site-header-logo-dosq-part">dosq</span>
+            <span className="site-header-logo-fm-part">.fm</span>
+          </Link>
+        </div>
+        <div className="site-header-user-actions">
+            <div className="site-header-nav">
+                <Link href="/concerts">Concerts</Link>
+                <Link href="/discovery">Discovery</Link>
+                <Link href="/social">Social</Link>
+            </div>
+            <div className="site-header-profile">
+                <Link href="/profile">
+                    <Image src="/images/user-circle.svg" alt="User Profile" width={40} height={40} />   
+                </Link>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
