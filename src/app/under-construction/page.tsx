9a33403e022b91a9c4f8e0af25e13a316371212f
dosqas <137123562+dosqas@users.noreply.github.com>
2025-03-15 22:55:23 +0200

    Updated the construction page to be nicer

--- src/app/under-construction/page.tsx
+++ src/app/under-construction/page.tsx
@@ -1,12 +1,23 @@
+"use client"
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 function UnderConstructionPage() {
+    const router = useRouter();
+
     return (
       <div className="construction-site-content">
-        <Image src="/images/page-under-construction.svg" alt="Page Under Construction" width={400} height={400} />
-        This page is under construction. Sit tight!
+        <div className="construction-site-image">
+          <Image src="/images/page-under-construction.svg" alt="Page Under Construction" width={400} height={400} />
+        </div>
+        <div className="construction-site-text">
+          <p>This page is under construction. Sit tight!</p>
+          <p>You can go back to the <Link href="/home" className="construction-page-link">home page</Link> or back to the <button onClick={() => router.back()} className="construction-page-link">previous page</button> you visited.</p>
+        </div>
       </div>
     );
-  }
-  
-  export default UnderConstructionPage;
+}
+
+export default UnderConstructionPage;
