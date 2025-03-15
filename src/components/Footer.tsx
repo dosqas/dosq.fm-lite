5800e70528d6f89e48fec01bedc0df0075c58375
dosqas <137123562+dosqas@users.noreply.github.com>
2025-03-15 20:22:00 +0200

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
     return (
         <footer className="site-footer">
             <div className="site-footer-content">
-                Created by Soptelea Sebastian (@dosqas) with &lt;3
+                <div className="site-footer-links">
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
+                    <Link href="/privacy-policy">Privacy Policy</Link>
+                </div>
+                <div className="site-footer-social-media">
+                    <Link href="https://www.linkedin.com/in/sebastian-soptelea/" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+                    <Link href="https://github.com/dosqas" target="_blank" rel="noopener noreferrer">GitHub</Link>
+                </div>
             </div>
         </footer>
     );
