5008817a613e0a1b94d4c825b30d575bfbe18825
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-02 03:54:18 +0300

    Add migration to include ProfileVideoPath column in Users table

---
+++ backend/Migrations/20250502005333_AddUserVideoUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserVideoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileVideoPath",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileVideoPath",
+                table: "Users");
+        }
+    }
+}
