b1c50e036c41d3e655167d7bb96fc1521b6c5930
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-02 03:25:16 +0300

    Update HealthController route to include API prefix

--- backend/Controllers/HealthController.cs
+++ backend/Controllers/HealthController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
     /// <summary>
